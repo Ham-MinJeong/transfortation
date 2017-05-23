e366e580bfcfd103fcad736e4a6ae2297d10e5c1
User <User@192.9.85.123>
2017-05-23 18:12:29 +0900

    사람 부분 수정

--- transfortation/src/transfortation/Person.java
+++ transfortation/src/transfortation/Person.java
@@ -4,5 +4,16 @@ public class Person {
 	public String name;		//이름
 	public Integer ages;	//나이
 	
+	public void 카드소지하기() {
+		System.out.println("카드 소지하기");
+	}
 
+	public void 카드충전하기() {
+		System.out.println("카드 충전하기");
+	}
+
+	public void 지하철타러가기() {
+		System.out.println("지하철 타러가기");
+	}
+	
 }

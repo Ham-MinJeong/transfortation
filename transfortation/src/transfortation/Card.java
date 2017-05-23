7932f86d285a4dc08176a04fca96103f53ad8552
User <User@192.9.85.123>
2017-05-23 17:58:48 +0900

    카드 부분 수정

--- transfortation/src/transfortation/Card.java
+++ transfortation/src/transfortation/Card.java
@@ -5,15 +5,15 @@ public class Card {
 	public Integer withdraw;	//출금액	
 	public Integer balance;		//잔액
 	
-	public void 카드소지하기(){
-	 System.out.println("카드 소지하기");
+	public void 카드소지하기() {
+		System.out.println("카드 소지하기");
 	}
-	
-	public void 카드충전하기(){
+
+	public void 카드충전하기() {
 		System.out.println("카드 충전하기");
 	}
-	
-	public void 지하철타러가기(){
+
+	public void 지하철타러가기() {
 		System.out.println("지하철 타러가기");
 	}
 }
